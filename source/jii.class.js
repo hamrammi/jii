0053731bc3fd4190b946a43e990b66b4b3c0dc29
nthrnth <hamrammi@gmail.com>
2013-01-12 19:35:06 +0400

    Changed core logic, now more flexible

--- source/jii.class.js
+++ source/jii.class.js
@@ -3,47 +3,52 @@
  *
  * Helper library that makes work with classes even better
  *
- * @version: 0.1.0 (last update: 27.12.2012)
+ * @version: 0.2.4 (last update: 12.01.2013)
  * @author: hamrammi@gmail.com
  */
+// TODO: private methods and variables
+// TODO: think about how to implement `instanceof` check when superClass is given
 (function() {
-  this.Class = (function() {
+  var root = this;
+
+  root.Class = (function() {
     var create = function() {
       var obj = {};
       var superClass = null;
 
-      // create regular class (can be superclass)
+      // Create regular class (can be superclass)
       if (typeof arguments[0] !== 'function' && typeof arguments[0] === 'object') {
         obj = arguments[0];
       }
 
-      // create subclass
+      // Create subclass
       if (typeof arguments[0] === 'function' && typeof arguments[1] === 'object') {
         superClass = arguments[0];
         obj = arguments[1];
+      } else if (typeof arguments[0] === 'function' && !arguments[1]) { // var subClass = Class.create(Parent);
+        superClass = arguments[0];
       }
 
-      var F = function() {
-
-        for (var i in obj) {
-          if (obj.hasOwnProperty(i)) {
-            F.prototype[i] = obj[i];
-          }
-        }
-
-        // When class is called with "new" keyword "this" points to class instance
-        F.prototype.init.apply(this, arguments);
+      var Class = function() {
+        // When class is called with `new` keyword `this` points to class instance
+        Class.prototype.init.apply(this, arguments);
       };
 
+      // Constructor function
+      Class.prototype.init = function() {};
+
+      // Reference to parent class
+      Class.prototype.superclass = Class;
+
       if (superClass) {
-        // create anonymous function
+        // Create an anonymous function
         var T = function() {};
-        // grab superClass prototype
+        // Grab superClass prototype
         T.prototype = superClass.prototype;
         // and assign it to class
-        F.prototype = new T();
+        Class.prototype = new T();
 
-        F.prototype.superclass = superClass;
+        Class.prototype.superclass = superClass;
 
         // At the moment superClass can see only one level inheritance
         // so superClass's subclasses array will contain only those
@@ -52,11 +57,11 @@
         // var C2 = Class.create(C1, { ... }); var C3 = Class.create(C1, { ... });
         // var C4 = Class.create(C2, { ... }); var C5 = Class.create(C3, { ... });
         // so C1.subclasses array will contain only C2 and C3 subclasses
-        superClass.subclasses.push(F);
+        superClass.subclasses.push(Class);
       }
 
       // TODO: implement if needed
-      F.addThis = function(obj) {
+      Class.addThis = function(obj) {
         for (var i in obj) {
           if (obj.hasOwnProperty(i)) {
             this[i] = obj[i];
@@ -64,29 +69,34 @@
         }
       };
 
-      // Prototype class with @param obj methods
+      // Prototype class with `@param obj` methods
       // so every instance of this class can use them
-      F.extend = function(obj) {
+      Class.extend = function(obj) {
         for (var i in obj) {
           if (obj.hasOwnProperty(i)) {
-            F.prototype[i] = obj[i];
+            Class.prototype[i] = obj[i];
           }
         }
       };
 
       // Subclasses array
-      F.subclasses = [];
+      Class.subclasses = [];
 
-      // Prevent inheriting superclass "init" function
-      F.prototype.init = function() {};
-      F.prototype.superclass = F;
-      F.prototype.constructor = F;
+      Class.prototype.constructor = Class;
+      // TODO: если класс входит в subclasses то запретить доступ к его свойствам и методам
+      Class.prototype.super = function(superClass) {
+        superClass = superClass || Class.prototype.superclass;
+        return superClass.prototype;
+      };
 
-      return F;
+      // Finally, extend class with given `@param obj`
+      Class.extend(obj);
+
+      return Class;
     };
 
     return {
       create: create
     }
   })();
-})();
+}).call(this);
